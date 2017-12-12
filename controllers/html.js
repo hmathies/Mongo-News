c3ef3aadfec089f49ac8e005b418c88de383d3e1
Heather <hlmathies@gmail.com>
2017-12-12 16:54:03 -0500

    Add require model

---
+++ controllers/html.js
@@ -0,0 +1,24 @@
+// *********************************************************************************
+// HTML ROUTES: ROUTES FOR SENDING USERS TO THE  HTML PAGES
+// *********************************************************************************
+// Dependencies
+// =============================================================
+var path = require("path");
+
+// Require all models
+var db = require("../models");
+// Routes
+// =============================================================
+module.exports = function(app) {
+  // Each of the below routes just handles the HTML page that the user gets sent to.
+
+  app.get('/news', function(req, res) {
+    res.render('news');
+  });
+  app.get('/', function(req, res) {
+    res.render('index');
+  });
+}
+// *********************************************************************************
+//                                END OF FILE
+// *********************************************************************************
--- public/javascript/news.js
+++ public/javascript/news.js
@@ -1,17 +1,45 @@
 $(() => {
+
+  // Loads results onto the page
+  function getComments() {
+
+    // Grab all of the comments
+    $.getJSON("/news", function(data) {
+      // For each comment...
+      for (var i = 0; i < data.length; i++) {
+        // ...populate #comments with a p-tag that includes the comments's author and a delete button
+        $("#comments").append("<h6 class='dataentry' data-id=" + data[i]._id + "><span class='newsTitle' data-id=" +
+          data[i]._id + ">" + data[i].title + "</span><span class=deleter>X</span></p>");
+      }
+    });
+  }
+
+  getComments();
   //click event for caputuring user and comment input
   $("#addComment").click((e) => {
     e.preventDefault();
-    var userComment = {
-      name: $("#name").val().trim(),
-      comment: $("#commentArea").val().trim()
-    }
-    console.log("This user posted: ", userComment)
-    addComment(userComment)
+    // var userComment = {
+    //   name: $("#name").val().trim(),
+    //   comment: $("#commentArea").val().trim()
+
+    // AJAX POST call to the submit route on the server
+    // This will take the data from the form and send it to the server
+    $.ajax({
+      type: "POST",
+      dataType: "json",
+      url: "/addComment",
+      data: {
+        name: $("#name").val().trim(),
+        comment: $("#commentArea").val().trim()
+      }
+    })
+
+    console.log("This user posted: ", data.name);
+    addComment(data);
   })
   //dynamically display user name and comment to the page
-  function addComment(userComment) {
-    $("#comments").append(`<h6> ${userComment.name} </h6> <p> ${userComment.comment} </p>`)
+  function addComment(data) {
+    $("#comments").append(`<h6> ${data.name} </h6> <p> ${data.comment} </p>`)
   }
   //click event to delete a comment--will need to change to delete only one comment
   $("#deleteComment").click((e) => {
